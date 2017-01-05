8e6a15e259912ac0e5aeb2db7726ea8626a3c74b
wxl <lzcrwxl@163.com>
2017-01-05 10:52:47 +0800

    merge

--- src/scripts/loadHtml.js
+++ src/scripts/loadHtml.js
@@ -1,22 +1,26 @@
 /**
  * Created by NHY on 2017/1/4.
  */
-$(function(){
-   $("#iframe").load(function(){
-       $.ajax({
-           type: "GET",
-           url: "http://abc.com/index.php/home/station/ajax_get_conf.html",
-           dataType: "json",
-           success:function(data){
-                if(data.code==0){
-
-                }else{
-                    alert("请求失败");
-                }
-           },
-           error:function(){
-               alert("请求错误");
-           }
-       });
-   });
+$(function () {
+  $("#iframe").load(function () {
+    $.ajax({
+      type: "GET",
+      url: "/index.php/home/station/ajax_get_conf.html",
+      dataType: "json",
+      success: function (data) {
+        if (data.code == 0) {
+          cc(data);
+        } else {
+          alert("请求失败");
+        }
+      },
+      error: function () {
+        alert("请求错误");
+      }
+    });
+    //代码从这开始
+    function cc(data){
+      console.log(data);
+    }
+  });
 });
